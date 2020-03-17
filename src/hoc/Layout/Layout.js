a3e7cff28c4046f9227c0d7f9c7d3913216d1a16
Anastasia Bokun <nastya.bokun@gmail.com>
2020-03-17 19:21:53 +0300

    using redux-saga

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,53 @@
+import React, {Component} from 'react';
+import { connect } from 'react-redux';
+
+import classes from './Layout.css';
+
+import Aux from '../Auxiliary/Auxiliary';
+import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
+import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
+
+class Layout extends Component{
+    state = {
+        showSideDrawer: false
+    };
+
+    sideDrawerClosedHandler = () => {
+        this.setState({showSideDrawer: false});
+    };
+
+    sideDrawerToggleHandler = () => {
+        this.setState((prevState) => {
+            return ({showSideDrawer: !prevState.showSideDrawer});
+        });
+    };
+
+    render() {
+        return (
+            <Aux>
+                <div>
+                    <Toolbar
+                        drawerToggleClicked={this.sideDrawerToggleHandler}
+                        isAuth={this.props.isAuthenticated}
+                    />
+                    <SideDrawer
+                        closed={this.sideDrawerClosedHandler}
+                        open={this.state.showSideDrawer}
+                        isAuth={this.props.isAuthenticated}
+                    />
+                </div>
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>
+        );
+    }
+}
+
+const mapStateToProps = state => {
+    return {
+        isAuthenticated: state.auth.token !== null
+    };
+};
+
+export default connect(mapStateToProps)(Layout);
